022f18b86da3dc4cd361f6c0ac342848a1c8c3c2
Srinivas <spg349@nyu.edu>
2017-11-27 14:46:43 -0500

    testing system calls

--- manticore/platforms/linux.py
+++ manticore/platforms/linux.py
@@ -2497,3 +2497,43 @@ class SLinux(Linux):
             'net': net.getvalue()
         }
         return ret
+
+    def sys_openat(self, buf, flags, mode):
+        '''
+        A version of open(2) that includes a special case for a symbolic path.
+        When given a symbolic path, it will create a temporary file with
+        64 bytes of symbolic bytes as contents and return that instead.
+
+        :param buf: address of zero-terminated pathname
+        :param flags: file access bits
+        :param mode: file permission mode
+        '''
+        offset = 0
+        symbolic_path = issymbolic(self.current.read_int(buf, 8))
+        if symbolic_path:
+            import tempfile
+            fd, path = tempfile.mkstemp()
+            with open(path, 'wb+') as f:
+                f.write('+'*64)
+            self.symbolic_files.append(path)
+            buf = self.current.memory.mmap(None, 1024, 'rw ', data_init=path)
+
+        rv = super(SLinux, self).sys_open(buf, flags, mode)
+
+        if symbolic_path:
+            self.current.memory.munmap(buf, 1024)
+
+        return rv
+
+    def sys_exit(self, error_code):
+        '''
+
+        :param self:
+        :return:
+        '''
+
+        if issymbolic(error_code):
+            logger.debug("Ask to read to a Error Code")
+            raise ConcretizeArgument(self, 0)
+
+        return super(SLinux, self).sys_exit(error_code)
